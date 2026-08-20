0a5661dd036e72c81286a53c76166e830bcd5cef
Alexandrebrr <corteis@gmail.com>
2026-08-20 15:18:26 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,2 @@
+export const metadata = { title: 'SupPlay' };
+export default function RootLayout({children}:{children:any}){return <html lang='pt-BR'><body style={{margin:0, background:'#080808', color:'#fff'}}>{children}</body></html>}
